85cee365b4b85b284a394229f9799a2d9eb37666
Eric Klicker <ericklicker@yahoo.com>
2016-09-13 15:35:58 -0700

    First commit

---
+++ HW 1 CS 172/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  HW 1 CS 172
+//
+//  Created by Eric Klicker on 9/8/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+void ex02()
+{
+    bool hasPassedTest = true;
+    srand((unsigned int)time(NULL));
+    int x = rand();
+    int y = rand();
+if (x > y)
+    cout<< "x is greater than y"<< endl;
+else
+    cout << "y is greater than x" <<endl;
+    
+ 
+}
+
+
+int main
+
+{
+    
+    
+    
+};
